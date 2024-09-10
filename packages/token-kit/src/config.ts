4a86b588d0087aa725b94d9ef32615d99f033ba3
Justin Feng <realfengjia@foxmail.com>
2024-09-10 18:27:04 +0800

    chore: 🎸 provide the default infura api key from ts-viewer as default

--- packages/token-kit/src/config.ts
+++ packages/token-kit/src/config.ts
@@ -1,4 +1,4 @@
-let infuraApiKey: string | undefined;
+let infuraApiKey = "9f79b2f9274344af90b8d4e244b580ef";
 export const TokenKit = {
   setInfuraApiKey: (apiKey: string) => {
     TokenKit.infuraApiKey = apiKey;
